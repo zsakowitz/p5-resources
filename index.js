80ed14829b95837cd9c2b15e2da889d80f145687
zsakowitz <zsakowitz@gmail.com>
2021-09-22 11:00:45 -0700

    Update files

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let vscode = require("vscode");
 /** @param {vscode.ExtensionContext} context */
 exports.activate = (context) => {
   let openGlitchProject = vscode.commands.registerCommand(
-    "p5.open-glitch",
+    "p5zs.open-glitch",
     () => vscode.env.openExternal("https://glitch.com/edit/#!/p5zs")
   );
 
   let remixGlitchProject = vscode.commands.registerCommand(
-    "p5.remix-glitch",
+    "p5zs.remix-glitch",
     () => vscode.env.openExternal("https://glitch.com/remix/#!/p5zs")
   );
 
